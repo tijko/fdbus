1093ea578606144529809516e37f2d2d738276d2
Tim Konick <konick781@gmail.com>
2015-06-06 00:26:40 -0400

    start server test

--- tests/fdbus_test.py
+++ tests/fdbus_test.py
@@ -12,12 +12,16 @@ class FdbusTest(unittest.TestCase):
     def setUp(self):
         self.server_path = '/tmp/fdbus_server'
         self.test_server = Server(self.server_path)
+        self.test_server.start()
 
     def tearDown(self):
        self.test_server.running = False
- 
-    def test_server(self):
+
+    def test_server_path(self):
         self.assertTrue(os.path.exists('/tmp/fdbus_server'))
+   
+    def test_server_running(self):
+        self.assertTrue(self.test_server.running) 
 
 
 if __name__ == '__main__':
