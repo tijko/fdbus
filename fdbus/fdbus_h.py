8c73360efacfbcece9aefbd9b2bc2b0aae1ecbee
Tim Konick <konick781@gmail.com>
2015-06-06 00:13:17 -0400

    edit comments

--- fdbus/fdbus_h.py
+++ fdbus/fdbus_h.py
@@ -87,17 +87,17 @@ class msghdr(Structure):
     def __init__(self, fd=None):
         ctrl_msg_len = CMSG_SPACE(sizeof(c_int))
         # If no 'fd' parameter is passed upon initialization, this is a header
-        # for a "receiver" call.  Otherwise this will initialized for a
+        # for a "receiver" call.  Otherwise the struct will be initialized for a
         # "sender" call, which will need a slightly different arrangement of
-        # the structure's fields.
+        # the struct's fields.
         #
-        # Of The "sender" and "receiver" 'msghdr's, the "sender" 'iovec' struct
+        # Of the "sender" and "receiver" `msghdr`s, the "sender" `iovec` struct
         # will be set with a minimal character array for the transmission over 
-        # `sendmsg` and the "receiver" 'iovec' will be initialized with a empty
+        # `sendmsg` and the "receiver" 'iovec' will be initialized with an empty
         # char array large enough to contain that message passed through 
         # `recvmsg`. 
         #
-        # With the cmsghdr its the similiar idea, where the "sender" will 
+        # With the `cmsghdr` its the similiar idea, where the "sender" will 
         # have the struct initialized with its data set and the "receiver"
         # will have an empty array assigned to its data field.
         if fd is None:
@@ -125,10 +125,10 @@ class cmsghdr(Structure):
 
 class cmsghdr_flex(Structure):
 
-    # This is a custom cmsghdr to use with the CMSG macros.  The only difference 
-    # between this struct and the cmsghdr struct above is that the 'cmsg_data' 
-    # field isn't in the former.  This is to account for the 'cmsg_data' field
-    # being a flex array and the macros calculating the size.
+    # This is a custom `cmsghdr` to use with the CMSG macros.  The only 
+    # difference between this struct and the cmsghdr struct above is that the 
+    # 'cmsg_data' field isn't in the former.  This is to account for the 
+    # 'cmsg_data' field being a flex array and the macros calculating the size.
 
     _fields_ = [('cmsg_len', c_int), 
                 ('cmsg_level', c_int), 
