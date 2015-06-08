a6a4dd2703ebc648e2d632b365e5b2ec6158f6ae
Tim Konick <konick781@gmail.com>
2015-06-08 11:44:02 -0400

    updated tests

--- tests/fdbus_test.py
+++ tests/fdbus_test.py
@@ -4,25 +4,31 @@
 import unittest
 import os
 
+from time import sleep
 from fdbus import *
 
 
 class FdbusTest(unittest.TestCase):
 
     def setUp(self):
-        self.server_path = '/tmp/fdbus_server'
+        self.server_path = '/tmp/fdbus_test_server'
         self.test_server = Server(self.server_path)
         self.test_server.start()
-
-    def tearDown(self):
-       self.test_server.running = False
-
+        sleep(.4)
+        self.test_client = Client(self.server_path) 
+        self.test_client.connect()
+        
     def test_server_path(self):
-        self.assertTrue(os.path.exists('/tmp/fdbus_server'))
+        self.assertTrue(os.path.exists(self.server_path))
    
     def test_server_running(self):
         self.assertTrue(self.test_server.running) 
 
+    def test_client_connection(self):
+        self.assertTrue(self.test_client.connected)
+
+    def tearDown(self):
+       self.test_server.running = False
 
 if __name__ == '__main__':
     unittest.main()
