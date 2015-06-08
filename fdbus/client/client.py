1d091213bf236f5ab34ff16feed59c825ed28bd5
Tim Konick <konick781@gmail.com>
2015-06-08 11:44:22 -0400

    client class

--- fdbus/client/client.py
+++ fdbus/client/client.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from select import *
+
 from ..fdbus_h import *
+from ..exceptions.exceptions import *
+from ..fdobjects.fd_object import FileDescriptor, FileDescriptorPool
 
 
 class Client(object):
@@ -10,14 +13,22 @@ class Client(object):
     def __init__(self, path):
         self.path = path
         self.client = libc.socket(AF_UNIX, SOCK_STREAM, PROTO_DEFAULT) 
+        if self.client == -1:
+            errno = get_errno()
+            raise SocketError(errno)
+        self.local_fds = FileDescriptorPool() 
+        self.connected = False
 
     def connect(self):
         self.client_address = pointer(sockaddr_un(AF_UNIX, self.path))
         self.client_address.contents.sun_family = AF_UNIX
         self.client_address.contents.sun_path = self.path
-        libc.connect(self.client, cast(self.client_address, POINTER(sockaddr)), 
-                     sizeof(sockaddr_un))
-
+        if (libc.connect(self.client, cast(self.client_address, 
+                         POINTER(sockaddr)), sizeof(sockaddr_un)) == -1):
+            errno = get_errno()
+            raise ConnectError(errno)
+        self.connected = True
+       
     def recvmsg(self):
         client_msghdr = pointer(msghdr())
         libc.recvmsg(self.client, client_msghdr, MSG_CMSG_CLOEXEC)
@@ -35,12 +46,20 @@ class Client(object):
     def closefd(self):
         pass
 
-    def openfd(self):
-        pass
+    def openfd(self, fdobj):
+        try:
+            fdobj.fopen()
+        except OpenError:
+            raise FileDescriptorError(fdobj)
+        return
 
-    def createfd(self):
-        pass
-
+    def createfd(self, path, mode):
+        name = path.split('/')[-1]
+        fdobj = FileDescriptor(path=path, mode=mode, name=name)
+        self.openfd(fdobj)
+        self.local_fds.add(fdobj)
+        return
+ 
     def passfd(self):
         pass
 
