e29e037c80ea30994f85f627d5c1b338c02b13bc
Tim Konick <konick781@gmail.com>
2015-06-09 00:15:39 -0400

    mock tests

--- tests/fdbus_test.py
+++ tests/fdbus_test.py
@@ -10,11 +10,13 @@ from fdbus import *
 
 class FdbusTest(unittest.TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
+        self.cwd = os.getcwd()
         self.server_path = '/tmp/fdbus_test_server'
         self.test_server = Server(self.server_path)
         self.test_server.start()
-        sleep(.4)
+        self.test_fd_path = os.path.join(self.cwd, 'test_fdbus_file')
         self.test_client = Client(self.server_path) 
         self.test_client.connect()
         
@@ -27,8 +29,26 @@ class FdbusTest(unittest.TestCase):
     def test_client_connection(self):
         self.assertTrue(self.test_client.connected)
 
-    def tearDown(self):
-       self.test_server.running = False
+    def test_client_createfd(self):
+        with open(self.test_fd_path, 'w+') as test_file:
+            test_file.write('fdbus test file')
+        self.test_client.createfd(self.test_fd_path, O_RDONLY)
+        fdpool = self.test_client.local_fds
+        self.assertTrue(len(fdpool.fdobjs) == 1)
+        # test the fd - name - path - mode
+
+    def test_server_pool(self):
+        pool = self.test_server.clients
+        self.assertTrue(len(pool.fd_pool) == 1)
+
+    def test_client_passfd(self):
+        pass
+
+    @classmethod
+    def tearDownClass(self):
+        self.test_server.running = False
+        os.unlink(self.test_fd_path)
+
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
