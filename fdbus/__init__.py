14d97741c58a1fd15aa928383fe9d802a3d92430
Tim Konick <konick781@gmail.com>
2015-06-17 12:33:03 -0400

    adjust fdobjects name

--- fdbus/__init__.py
+++ fdbus/__init__.py
@@ -1,9 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
 from client.client import Client
 from server.server import Server
 from fdbus_h import *
-from fdobjects.fd_object import *
+from fdobjects.fdobjects import * 
 from exceptions.exceptions import *
