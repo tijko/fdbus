5e4b16a80f5a3223e1897ccc8dfeabcdb41974bd
Tim Konick <konick781@gmail.com>
2015-06-30 21:00:20 -0400

    use test suites

--- tests/fdbus_test.py
+++ tests/fdbus_test.py
@@ -8,7 +8,7 @@ from time import sleep
 from fdbus import *
 
 
-class FdbusTest(unittest.TestCase):
+class FDBusServerTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
@@ -21,14 +21,67 @@ class FdbusTest(unittest.TestCase):
         self.test_fd_name = 'test_fdbus_file'
         self.test_fd_path = os.path.join(self.cwd, self.test_fd_name)
         self.test_client = Client(self.server_path) 
-        self.test_client.connect()
-        
+        try:
+            self.test_client.connect()
+        except ConnectError:
+            self.test_server.running = False
+            
     def test_server_path(self):
         self.assertTrue(os.path.exists(self.server_path))
    
     def test_server_running(self):
         self.assertTrue(self.test_server.running) 
 
+    def test_server_pool(self):
+        self.assertTrue(len(self.test_server.clients) == 1)
+
+    def test_server_clientfd_pool(self):
+        client = self.test_server.fdpool.client_fdobjs
+        self.assertTrue(len(client) == 1)
+
+    def test_server_recvfd(self):
+        with open(self.test_fd_path, 'w+') as test_file:
+            test_file.write('fdbus test file')
+        self.test_client.createfd(self.test_fd_name, O_RDONLY)
+        self.test_client.loadfd(self.test_fd_name)
+        fdpool = self.test_server.fdpool.fdobjs
+        self.assertTrue(len(fdpool) == 1)
+
+    def test_server_clientpool(self):
+        pool = self.test_server.current_clients
+        self.assertTrue(len(pool) == 1)
+    """
+    def test_server_removeclient(self):
+        pool_w_client = self.test_server.current_clients
+        client = pool_w_client[0]
+        self.test_server.remove_client(client)
+        pool_wo_client = self.test_server.current_clients
+        self.assertTrue(client not in pool_wo_client)
+    """
+    @classmethod
+    def tearDownClass(self):
+        self.test_server.running = False
+        #os.unlink(self.test_fd_path)
+
+
+class FDBusClientTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.cwd = os.getcwd()
+        self.server_path = '/tmp/fdbus_test_server'
+        if os.path.exists(self.server_path):
+            os.unlink(self.server_path)
+        self.test_server = Server(self.server_path)
+        self.test_server.start()
+        self.test_fd_name = 'test_fdbus_file'
+        self.test_fd_path = os.path.join(self.cwd, self.test_fd_name)
+        self.test_client = Client(self.server_path) 
+        try:
+            self.test_client.connect()
+        except ConnectError:
+            self.test_server.running = False
+            
     def test_client_connection(self):
         self.assertTrue(self.test_client.connected)
 
@@ -53,43 +106,25 @@ class FdbusTest(unittest.TestCase):
         pool = self.test_client.fdpool
         test_fd = pool.fdobjs[self.test_fd_name][1]
         self.assertTrue(test_fd.mode == O_RDONLY)
-
-    def test_server_pool(self):
-        self.assertTrue(len(self.test_server.clients) == 1)
-
-    def test_server_clientfd_pool(self):
-        client = self.test_server.fdpool.client_fdobjs
-        self.assertTrue(len(client) == 1)
-
-    def test_client_loadfd(self):
-        self.test_client.loadfd(self.test_fd_name)
-
-    def test_server_recvfd(self):
-        fdpool = self.test_server.fdpool.fdobjs
-        self.assertTrue(len(fdpool) == 1)
-
+    """
     def test_server_remove_clientfd(self):
         self.test_client.remove(self.test_fd_name)
         fdpool = self.test_server.fdpool.fdobjs
-
-    def test_server_clientpool(self):
-        pool = self.test_server.current_clients
-        self.assertTrue(len(pool) == 1)
-
-    def test_server_removeclient(self):
-        pool_w_client = self.test_server.current_clients
-        client = pool_w_client[0]
-        self.test_server.remove_client(client)
-        pool_wo_client = self.test_server.current_clients
-        self.assertTrue(client not in pool_wo_client)
-
+        self.assertTrue(len(fdpool) == 0)
+    """ 
     @classmethod
     def tearDownClass(self):
         self.test_server.running = False
-        os.unlink(self.test_fd_path)
 
 
 if __name__ == '__main__':
-    loader = unittest.TestLoader()
-    loader.loadTestsFromTestCase(FdbusTest)
-    unittest.main(testLoader=loader, verbosity=2)
+    client = unittest.TestLoader()
+    server = unittest.TestLoader()
+    suiteClient = client.loadTestsFromTestCase(FDBusClientTest)
+    suiteServer = server.loadTestsFromTestCase(FDBusServerTest)
+    suite = unittest.TestSuite()
+    suite.addTests([suiteClient, suiteServer])
+    result = unittest.TestResult()
+    suite.run(result)
+    print result
+    
