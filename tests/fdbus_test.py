b5e22d101087a168bdf091d75140458591c3eea4
Tim Konick <konick781@gmail.com>
2015-08-13 01:27:14 -0400

    Updated tests

--- tests/fdbus_test.py
+++ tests/fdbus_test.py
@@ -23,17 +23,6 @@ class FDBusServerTest(unittest.TestCase):
         fdpool = test_server.fdpool.fdobjs
         self.assertTrue(len(fdpool) == 1)
 
-
-class FDBusServerClosingTest(unittest.TestCase):
-
-    def test_server_removeclient(self):
-        pool_w_client = test_server.current_clients
-        client = pool_w_client[0]
-        test_server.remove_client(client)
-        pool_wo_client = test_server.current_clients
-        self.assertTrue(client not in pool_wo_client)
-
-
 class FDBusClientTest(unittest.TestCase):
 
     def test_client_connection(self):
@@ -58,34 +47,12 @@ class FDBusClientTest(unittest.TestCase):
         test_fd = pool.fdobjs[test_fd_name][1]
         self.assertTrue(test_fd.mode == O_RDONLY)
 
-    def test_client_peers(self):
-        test_client.getpeers()
-        peers = test_client.peers
-        self.assertTrue(len(peers) == 1)
-
+class FDBusClientPeersTest(unittest.TestCase):
+    pass
 
 class FDBusClientClosingTest(unittest.TestCase):
+    pass
 
-    def test_server_remove_clientfd(self):
-        test_client.remove(test_fd_name)
-        sleep(1)
-        pool = test_server.fdpool.fdobjs
-        self.assertTrue(len(pool) == 0)
-
-
-def display_test_errors(errors):
-    for error in errors:
-        print ''.join(['\n' + str(error[0]) + '\n'] + list(error[1:]))
-
-def run_test(test):
-    suite = unittest.TestSuite()
-    result = unittest.TestResult()
-    result.buffer = True
-    suite.addTest(test)
-    suite.run(result)
-    print result
-    map(display_test_errors, [result.errors, result.failures])
-    
 
 if __name__ == '__main__':
     cwd = os.getcwd()
@@ -97,22 +64,13 @@ if __name__ == '__main__':
     test_fd_name = 'test_fdbus_file'
     test_fd_path = os.path.join(cwd, test_fd_name)
     test_client = Client(server_path) 
+    sleep(2)
     test_client.connect()
     with open(test_fd_path, 'w+') as test_file:
         test_file.write('fdbus test file')
     test_client.createfd(test_fd_path, O_RDONLY)
     test_client.loadfd(test_fd_name)
-    client = unittest.TestLoader()
-    server = unittest.TestLoader()
-    clientcls = unittest.TestLoader()
-    servercls = unittest.TestLoader()
-    suiteClient = client.loadTestsFromTestCase(FDBusClientTest)
-    suiteServer = server.loadTestsFromTestCase(FDBusServerTest)
-    suiteClientcls = clientcls.loadTestsFromTestCase(FDBusClientClosingTest)
-    suiteServercls = servercls.loadTestsFromTestCase(FDBusServerClosingTest)
-    tests = [suiteServer, suiteClient, suiteClientcls, suiteServercls]
-    for test in tests:
-        sleep(1)
-        run_test(test)
+    unittest.main(verbosity=3, exit=False)
+    test_client.connected = False
     test_server.running = False
     os.remove(test_fd_path)
